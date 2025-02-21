66485a05b69b1c5d2020c88c97d48187ca471df8
lamphuoc <phuocnguyenlam3110@gmail.com>
2025-02-21 18:06:20 +0700

    update

--- src/View/Input.java
+++ src/View/Input.java
@@ -1,7 +1,9 @@
 // Purpose: Get input from user.
 package View;
-
+import Utils.Acceptable;
 import java.util.Scanner;
+import java.util.jar.Attributes.Name;
+
 import Models.Student;
 /**
  *
@@ -10,15 +12,74 @@ import Models.Student;
 public class Input {
     
     private static final Scanner scanner = new Scanner(System.in);   
-
     
-    public void getRegister(Student student){
-        student.setStudentID(scanner.nextLine());
-        student.setName(scanner.nextLine());
-        student.setPhoneNumber(scanner.nextLine());
-        student.setEmail(scanner.nextLine());
-       
+    public static boolean isValid(String input, String regex) {
+        return input.matches(regex);
     }
+    
+    // public void getRegister(Student student){
+    //     student.setStudentID(scanner.nextLine());
+    //     student.setName(scanner.nextLine());
+    //     student.setPhoneNumber(scanner.nextLine());
+    //     student.setEmail(scanner.nextLine());
+       
+    // }
+    public static String getStuName(){
+        String name = "";
+        System.out.println("Enter name: ");
+        name = scanner.nextLine();
+        while(true){
+            if(isValid(name, Acceptable.NAME_VALID)){
+                break;
+            }else{
+                System.out.println("Invalid name. Please enter again: ");
+            }
+        }
+        return name;
+    }
+    public static String getStuID(){
+        String iD = "";
+        System.out.println("Enter name: ");
+        iD = scanner.nextLine();
+        while(true){
+            if(isValid(iD, Acceptable.STUDENT_ID)){
+                break;
+            }else{
+                System.out.println("Invalid student ID. Please enter again: ");
+            }
+        }
+        return iD;
+    }
+    
+    public static String getStuPhone(){
+        String phone = "";
+        System.out.println("Enter phone number: ");
+        phone = scanner.nextLine();
+        while(true){
+            if(isValid(phone, Acceptable.PHONE_VALID)){
+            break;
+            }else{
+            System.out.println("Invalid phone number. Please enter again: ");
+            }
+        }
+        return phone;
+        }
+        
+        
+    public static String getStuEmail(){
+        String email = "";
+        System.out.println("Enter email: ");
+        email = scanner.nextLine();
+        while(true){
+            if(isValid(email, Acceptable.EMAIL_VALID)){
+                break;
+                }else{
+                System.out.println("Invalid email. Please enter again: ");
+                }
+            }
+        return email;
+        }
+    // filter by campus: SE, HE, DE, QE, CE
     public void printCampus(){
        System.out.println("1. SE");
        System.out.println("2. HE");
